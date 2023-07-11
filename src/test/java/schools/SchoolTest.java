65a917663b47c9efc830f7d8935a03892cdd2fa9
Anna Książek <aksiazek@softak.eu>
2023-07-11 19:52:25 +0200

    add tests hints

--- src/test/java/schools/SchoolTest.java
+++ src/test/java/schools/SchoolTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.security.KeyStore;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
+// staramy sie nie mieszac asercji i nie uzywamy * - eliminacja tego jest do ustawienia w formatterze
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchoolTest {
@@ -27,53 +25,79 @@ class SchoolTest {
         school.getSchoolClasses().clear();
     }
 
+    /**
+     * uwaga ogolna - za mala probla danych testowych i ich przypadkow. Czy na pewno dobrze zadzialaja metody liczace ucznikow jak bedzie ich wiecej niz 1 w klasie
+     * i wiecej klas? Na razie nie ma takiej pewnosci.
+     * A co te klasy maja zwrocic jak nie ma zadnej klasy?
+     * Czy w ogole mozna utworzyc szkole bez klasy?
+     * Czy mozna dodac klase bez zadnego ucznia?
+     *
+     * Jakiekolwiek sa odpowiedzi na ww pytania powinny byc dla nich testy.
+     */
     @Test
-    public void shouldReturnFalseIfClassExist(){
+    // dobrze by nazwy reprezentowaly efekt biznesowy a nie techniczny. Oczywiscie czasem sie nie da ale akurat tutaj
+    // jest mozliwosc opisania efektu biznesowego
+    public void shouldNotLetAddTheSameClassMoreThanOne(){
         // given
         school.addClass(clazzA);
+        // jak bedzie taka operacja robiona na klasach to musimy je zwrocic
+        // a z opisu w klasie School nie wynika, ze to jest oczekiwane
+
+        // assertThat(school.howManyClasses()).isEqual(1);
 
         // when
         boolean result = school.addClass(clazzA);
 
         // then
         assertFalse(result);
+        // assertThat(school.howManyClasses()).isEqual(1);
     }
     @Test
-    public void shouldReturnTrueIfClassCanBeAdded(){
+    public void shouldAddNewClassToSchool(){
         // given
         school.addClass(clazzA);
+        // assertThat(school.howManyClasses()).isEqual(0);
 
         // when
+        // to nie jest najszczesliwsza logika
+        // lepiej by bylo dodac
+        // school.hasClass(clazzA);
         boolean result = school.getSchoolClasses().containsValue(clazzA);
 
         // then
+
+        // to sprawdzenie jest ok
         assertTrue(result);
         assertThat(school.getSchoolClasses()).hasSize(1);
     }
     @Test
-    public void shouldReturnTrueIfRemovingSchoolClassSuccessful(){
+    public void shouldRemoveExistingClass(){
         // given
         school.addClass(clazzA);
 
         // when
+        // o nie jest test tego co bylo opisane wyzej - to jest nadal element given i lepiej by bylo zrobic to przez
+        // hasClass
         assertThat(school.getSchoolClasses()).containsValue(clazzA);
 
         // then
-        assertTrue(school.removeClass(clazzA));
+        // to jest when i lepiej by wygladalo
+        var result = school.removeClass(clazzA);
+
+        // then wlasciwe - do poprawy asercja na assertThat()
+        assertTrue(result);
         assertThat(school.getSchoolClasses()).hasSize(0);
     }
     @Test
-    public void shouldReturnFalseIfRemovingSchoolClassDoNotExist(){
+    public void shouldNotRemoveClassIfNotInSchoolAndLetContinue(){
         // given
-        school.addClass(clazzA);
-        assertThat(school.getSchoolClasses()).containsValue(clazzA);
-        school.removeClass(clazzA);
+        // tu bym nic nie robila, bo wtedy na pewno szkola nie ma klasy
 
         // when
-        assertThat(school.getSchoolClasses().size()).isEqualTo(0);
+        var result = school.removeClass(clazzA);
 
         // then
-        assertFalse(school.removeClass(clazzA));
+        assertFalse(result);
     }
     @Test
     public void shouldReturnTotalSchoolHeadcount(){
@@ -81,22 +105,30 @@ class SchoolTest {
         school.addClass(clazzA);
 
         // when
+        // cos tutaj jest nie w porzadku - getHeadcount nie powinna miec argumentu
+        // tak byloby lepiej
+        // var resultAk = school.getHeadcount();
         boolean result = school.getHeadcount(school.getSchoolClasses()) == 1;
 
         // then
         assertTrue(result);
         assertThat(school.getSchoolClasses().size()).isEqualTo(1);
         // która z tych dwóch asercji powyżej jest lepsza do sprawdzenia żądanej metody?
+
+        // wg mnie taka :)
+        // assertThat(resultAk).isEqual(1);
     }
     @Test
-    public void shouldReturnMapOfClassesWithHeadcount(){
+    public void shouldReturnHeadcountPerClass(){
         // given
         school.addClass(clazzA);
 
         // when
-        school.getEachClassHeadcount(school.getSchoolClasses());
+        var result = school.getEachClassHeadcount(school.getSchoolClasses());
 
         //then
-     //   assertTrue(schoolClassesWithHeadcountMap.size() == 1);
+//        assertTrue(schoolClassesWithHeadcountMap.size() == 1);
+        assertThat(result).isNotNull()
+                .hasSize(1);
     }
 }
